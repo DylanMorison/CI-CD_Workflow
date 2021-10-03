7a659701d2d92156d0ca61afa070d39747a0f059
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2021-10-03 14:42:07 -0700

    remove semantic release

--- client/release.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/DylanMorison/CI-CD_Workflow",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
-}
